999dc2b47fbf25d36fd07510a70574f2acf81846
Michael Irwin <mike.irwin1@gmail.com>
2019-02-25 14:56:59 +0000

    finished

---
+++ src/main/java/com/example/bootstrap/controllers/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.example.bootstrap.controllers;
+
+import com.example.bootstrap.models.Employee;
+import com.example.bootstrap.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/employees")
+
+public class EmployeeController {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Optional<Employee> getEmployee(@PathVariable Long id){
+        return employeeRepository.findById(id);
+    }
+}
